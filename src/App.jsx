302d3e764f173984328cfc2a47fe91b9c86f7509
Oleksii0210 <141589602+Oleksii0210@users.noreply.github.com>
2024-06-14 00:19:00 +0300

    fix

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import transactions from "./transactions.json";
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        username={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+};
+
+export default App;
